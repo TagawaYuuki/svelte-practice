5c89b9b574d8b0269dde2bf73bfa8385a9b10ed7
haya1007 <hayahaya.cha@gmail.com>
2025-06-23 22:21:47 +0900

    add

--- blog/src/routes/+page.svelte
+++ blog/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<main>
+    <header>
+        <a href="/test" rel="">test</a>
+    </header>
+</main>
+
+<style>
+    main {
+        background-color: aqua;
+    }
+
+    a {
+        font-size: 64px;
+    }
+</style>
